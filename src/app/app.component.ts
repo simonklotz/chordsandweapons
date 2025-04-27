62d8b27eb713a7ae73cdadc98a4294f48112a3ff
Simon Klotz <simon.klotz@posteo.de>
2025-04-27 18:21:52 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  template: `<h1>Hello World!</h1>`,
+})
+export class AppComponent {}
