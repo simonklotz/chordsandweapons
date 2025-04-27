dc20ad19a36add7b190ebab20abed3a010060f62
Simon Klotz <simon.klotz@posteo.de>
2025-04-27 21:53:23 +0200

    layout: implements some basic presentional components for header, main & footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/layout/header.component';
+import { MainComponent } from './shared/layout/main.component';
+import { FooterComponent } from './shared/layout/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<h1>Hello World!</h1>`,
+  imports: [RouterOutlet, HeaderComponent, MainComponent, FooterComponent],
+  template: `
+    <div class="page-container">
+      <app-header class="app-header"></app-header>
+      <app-main class="app-main"></app-main>
+      <app-footer class="app-footer"></app-footer>
+    </div>
+  `,
 })
 export class AppComponent {}
---
+++ src/app/shared/layout/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-header',
+  template: `
+    <header class="header">
+      <h1 class="h1">Hello World!</h1>
+    </header>
+  `,
+})
+export class HeaderComponent {}
