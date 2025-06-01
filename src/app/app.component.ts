4d982e547adcdf228f4122b7d13f546d5fdc10bd
Simon Klotz <simon.klotz@datagroup.de>
2025-06-01 17:24:34 +0200

    layout: adds some basic layout scss to visualize the header, main & footer components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/layout/header.component';
-import { MainComponent } from './shared/layout/main.component';
-import { FooterComponent } from './shared/layout/footer.component';
+import { HeaderComponent } from './shared/ui-common/layout/header.component';
+import { MainComponent } from './shared/ui-common/layout/main.component';
+import { FooterComponent } from './shared/ui-common/layout/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, MainComponent, FooterComponent],
   template: `
-    <div class="page-container">
-      <app-header class="app-header"></app-header>
-      <app-main class="app-main"></app-main>
-      <app-footer class="app-footer"></app-footer>
-    </div>
+    <app-header class="app-header"><h1 class="h1">Header</h1></app-header>
+    <app-main class="app-main"><h1 class="h1">Main</h1></app-main>
+    <app-footer class="app-footer"><h1 class="h1">Footer</h1></app-footer>
   `,
 })
 export class AppComponent {}
