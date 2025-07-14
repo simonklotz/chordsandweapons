c60197b81888225f79ce93a7eb0ebcc0f1e998af
Simon Klotz <simon.klotz@datagroup.de>
2025-07-14 11:57:56 +0200

    layout: implements some basic presentional components for header, main & footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header.component';
+import { MainComponent } from './layout/main.component';
+import { FooterComponent } from './layout/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: `<h1>{{ title() }}</h1>`,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, MainComponent, FooterComponent],
+  template: `
+    <app-header class="app-header"><h1 class="h1">Header</h1></app-header>
+    <app-main class="app-main"><h1 class="h1">Main</h1></app-main>
+    <app-footer class="app-footer"><h1 class="h1">Footer</h1></app-footer>
+  `,
 })
 export class App {
   protected readonly title = signal('chordsandweapons');
