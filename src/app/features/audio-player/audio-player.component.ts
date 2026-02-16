29a55f203f41430d2651c998c4f15c341e3fb87a
Simon Klotz <simon.klotz@posteo.de>
2026-02-16 17:26:05 +0100

    feature(audio-player): implements seeking and visualization of playback progress

--- src/app/features/audio-player/audio-player.component.ts
+++ src/app/features/audio-player/audio-player.component.ts
@@ -1,18 +1,26 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, ElementRef, inject, signal, Signal, viewChild } from '@angular/core';
+import { Router } from '@angular/router';
 import { CartButtonComponent } from '../../shared/components/cart-button.component';
+import { numberToCurrency } from '../../shared/helpers/number-to-currency';
 import { CartService } from '../cart/cart.service';
 import { ProductApiService } from '../product/product-api.service';
 import { PreviousButtonComponent } from './components/previous-button.component';
 import { NextButtonComponent } from './components/next-button.component';
 import { PlayButtonComponent } from './components/play-button.component';
 import { AudioPlayerService } from './audio-player.service';
-import { Router } from '@angular/router';
-import { numberToCurrency } from '../../shared/helpers/number-to-currency';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-audio-player',
   standalone: true,
-  imports: [PreviousButtonComponent, NextButtonComponent, PlayButtonComponent, CartButtonComponent],
+  imports: [
+    PreviousButtonComponent,
+    NextButtonComponent,
+    PlayButtonComponent,
+    CartButtonComponent,
+    NgClass,
+  ],
   template: `
     @if (audioPlayer.currentTrack(); as track) {
       <div class="audio-player">
@@ -22,37 +30,58 @@ import { numberToCurrency } from '../../shared/helpers/number-to-currency';
             class="audio-player__artwork"
             [src]="artwork"
             alt="artwork"
-            width="50px"
-            height="50px"
+            width="60px"
+            height="60px"
             (click)="navigateToDetailView()"
             (keydown.enter)="navigateToDetailView()"
             (keydown.space)="navigateToDetailView()"
           />
         }
 
-        <div class="audio-player__track-title">
-          {{ track.title }}
-        </div>
+        <div class="audio-player__player">
+          <div class="player__controls">
+            <app-previous-button
+              [disabled]="!audioPlayer.hasPrevious()"
+              (clicked)="audioPlayer.previous()"
+            ></app-previous-button>
+            <app-play-button
+              [isPlaying]="audioPlayer.isPlaying()"
+              (clicked)="audioPlayer.togglePlayPause()"
+            ></app-play-button>
+            <app-next-button
+              [disabled]="!audioPlayer.hasNext()"
+              (clicked)="audioPlayer.next()"
+            ></app-next-button>
+          </div>
 
-        <div class="audio-player__controls">
-          <app-previous-button
-            [disabled]="!audioPlayer.hasPrevious()"
-            (clicked)="audioPlayer.previous()"
-          ></app-previous-button>
-          <app-play-button
-            [isPlaying]="audioPlayer.isPlaying()"
-            (clicked)="audioPlayer.togglePlayPause()"
-          ></app-play-button>
-          <app-next-button
-            [disabled]="!audioPlayer.hasNext()"
-            (clicked)="audioPlayer.next()"
-          ></app-next-button>
-        </div>
+          <div class="player__position">
+            {{ track.position + 1 }}/{{ audioPlayer.playlist()?.tracks?.length }}
+          </div>
 
-        <div class="audio-player__progress-bar">PROGRESS BAR (tba)</div>
+          <div class="player__time">{{ currentTime() }}</div>
 
-        <div class="audio-player__position">
-          {{ track.position + 1 }}/{{ audioPlayer.playlist()?.tracks?.length }}
+          <div #trackTitle class="player__track-title">
+            <div
+              class="animation-wrapper"
+              [ngClass]="moveTitle() ? 'animation-running' : 'animation-paused'"
+            >
+              <span class="track-title-text">{{ track.title }}</span>
+            </div>
+          </div>
+
+          <div class="player__progress-bar">
+            <input
+              id="audio-progress"
+              #audioProgress
+              class="player__progress-bar-input"
+              type="range"
+              min="0"
+              max="100"
+              [value]="progress()"
+              [style]="'--progress:' + progress() + '%'"
+              (input)="updateRangeProgress(audioProgress)"
+            />
+          </div>
         </div>
 
         <div
@@ -78,16 +107,30 @@ export class AudioPlayerComponent {
 
   readonly audioPlayer = inject(AudioPlayerService);
 
+  trackTitleElement = viewChild<ElementRef<HTMLDivElement>>('trackTitle');
+  moveTitle = signal(false);
+
   get price(): string | undefined {
     return numberToCurrency(Number(this.audioPlayer.playlist()?.productPrice.amount));
   }
 
-  get currencyCode(): string | undefined {
-    return this.audioPlayer.playlist()?.productPrice.currencyCode;
-  }
-
   get productId(): number | undefined {
     return this.audioPlayer.playlist()?.productId;
+  }
+
+  get progress(): Signal<number> {
+    return this.audioPlayer.progress;
+  }
+
+  readonly currentTime = computed<string>(() => {
+    const minutes = Math.floor(this.audioPlayer.currentTime() / 60);
+    const seconds = Math.floor(this.audioPlayer.currentTime() % 60);
+    const displayedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${minutes}:${displayedSeconds}`;
+  });
+
+  constructor() {
+    this.moveOnTitleOverflow();
   }
 
   navigateToDetailView(): void {
@@ -95,6 +138,26 @@ export class AudioPlayerComponent {
       return;
     }
     this._router.navigate(['release', this.productId]);
+  }
+
+  moveOnTitleOverflow(): void {
+    toObservable(this.audioPlayer.playbackState).subscribe(() => {
+      const trackTitle = this.trackTitleElement()?.nativeElement;
+      if (trackTitle && trackTitle?.scrollWidth > trackTitle?.clientWidth) {
+        this.moveTitle.set(true);
+      } else {
+        this.moveTitle.set(false);
+      }
+    });
+  }
+
+  updateRangeProgress(input: HTMLInputElement): void {
+    const val = parseFloat(input.value) || 0;
+    const progress = (val / 100) * 100;
+
+    input.style.setProperty('--progress', progress + '%');
+
+    this.audioPlayer.seekToPercentage(Number(input.value));
   }
 
   addToCart(): void {
--- src/app/shared/components/cart-button.component.ts
+++ src/app/shared/components/cart-button.component.ts
@@ -4,11 +4,7 @@ import { Component, computed, input, output } from '@angular/core';
   selector: 'app-cart-button',
   standalone: true,
   template: `
-    <button
-      class="cart-button"
-      [disabled]="disabled()"
-      (click)="onClick($event)"
-    >
+    <button class="cart-button" [disabled]="disabled()" (click)="onClick($event)">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 -960 960 960"
@@ -17,7 +13,7 @@ import { Component, computed, input, output } from '@angular/core';
         class="cart-icon"
       >
         <path
-          d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z"
+          d="M440-600v-120H320v-80h120v-120h80v120h120v80H520v120h-80ZM223.5-103.5Q200-127 200-160t23.5-56.5Q247-240 280-240t56.5 23.5Q360-193 360-160t-23.5 56.5Q313-80 280-80t-56.5-23.5Zm400 0Q600-127 600-160t23.5-56.5Q647-240 680-240t56.5 23.5Q760-193 760-160t-23.5 56.5Q713-80 680-80t-56.5-23.5ZM40-800v-80h131l170 360h280l156-280h91L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68.5-39t-1.5-79l54-98-144-304H40Z"
         />
       </svg>
     </button>
